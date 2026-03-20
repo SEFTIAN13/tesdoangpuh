83127528a856d11bca4967aed3d2ec52d469117a
seftian <ferdiansyahseftian6@gmail.com>
2026-03-21 02:00:44 +0700

    menambahkan foto

---
+++ supabase.config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://ggfdzrkrbghwvequzorv.supabase.co';  // GANTI INI
+const SUPABASE_ANON_KEY = 'sb_publishable_rNIcT3N-zpgBkqyFAePbYw_y1KyDGkq';  // GANTI INI
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
