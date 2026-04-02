d559efff55de6db0ca484c0493593df339a16bcf
seftian <ferdiansyahseftian6@gmail.com>
2026-04-02 21:11:57 +0700

    mengubah tampilan hp

--- supabase.config.js
+++ supabase.config.js
@@ -1,7 +1,5 @@
-const SUPABASE_URL = 'https://ggfdzrkrbghwvequzorv.supabase.co';  // GANTI INI
-const SUPABASE_ANON_KEY = 'sb_publishable_rNIcT3N-zpgBkqyFAePbYw_y1KyDGkq';  // GANTI INI
+const SUPABASE_URL = 'https://ggfdzrkrbghwvequzorv.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_rNIcT3N-zpgBkqyFAePbYw_y1KyDGkq';
 
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-
 console.log('Supabase connected!');
